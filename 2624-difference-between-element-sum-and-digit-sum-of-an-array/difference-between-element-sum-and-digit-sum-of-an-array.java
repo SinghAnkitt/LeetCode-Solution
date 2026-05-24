caf53c411865bb632a5f7647a8405ac390519f64
Ankit Kumar Singh <148609982+SinghAnkitt@users.noreply.github.com>
2026-05-24 15:12:30 +0530

    Time: 2 ms (99.52%) | Memory: 46.7 MB (12.60%) - LeetSync

--- 2624-difference-between-element-sum-and-digit-sum-of-an-array/difference-between-element-sum-and-digit-sum-of-an-array.java
+++ 2624-difference-between-element-sum-and-digit-sum-of-an-array/difference-between-element-sum-and-digit-sum-of-an-array.java
@@ -1,20 +1,36 @@
+// class Solution {
+//     public int differenceOfSum(int[] nums) {
+//         int sum = 0;
+//         int sum1 = 0;
+//         for(int i = 0; i < nums.length; i++){
+//             sum += nums[i];
+//         }
+//         for(int i = 0; i < nums.length; i++){
+//             if(nums[i] < 10) sum1 += nums[i];
+//             else{
+//             while( nums[i] > 0){
+//                 int digit = nums[i] % 10;
+//                 sum1 += digit;
+//                 nums[i] /= 10;
+//                 }
+//             }
+//         }
+//         return sum - sum1;
+//     }
+// }
+
 class Solution {
     public int differenceOfSum(int[] nums) {
-        int sum = 0;
-        int sum1 = 0;
-        for(int i = 0; i < nums.length; i++){
-            sum += nums[i];
-        }
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] < 10) sum1 += nums[i];
-            else{
-            while( nums[i] > 0){
-                int digit = nums[i] % 10;
-                sum1 += digit;
-                nums[i] /= 10;
-                }
+        int elementSum = 0;
+        int digitSum = 0;
+        for(int num : nums){
+            elementSum += num;
+            int n = num;
+            while(n > 0){
+                digitSum += n % 10;
+                n /= 10;
             }
         }
-        return sum - sum1;
+        return Math.abs(elementSum - digitSum);
     }
 }
