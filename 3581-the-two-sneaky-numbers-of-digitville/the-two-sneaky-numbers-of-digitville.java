9aab1681af9c25730fd15c5dac618bc44ee1892c
Ankit Kumar Singh <148609982+SinghAnkitt@users.noreply.github.com>
2026-05-24 11:06:43 +0530

    Time: 1 ms (99.94%) | Memory: 45.4 MB (91.48%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
@@ -1,14 +1,32 @@
+// class Solution {
+//     public int[] getSneakyNumbers(int[] nums) {
+//         Arrays.sort(nums);
+//         int[] ans = new int[2];
+//         int count = 0;
+//         for(int i = 0; i < nums.length-1; i++){
+//             if(nums[i] == nums[i+1]) {
+//                 ans[count] = nums[i];
+//                 count++;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution {
     public int[] getSneakyNumbers(int[] nums) {
-        Arrays.sort(nums);
+        int[] freq = new int[nums.length];
         int[] ans = new int[2];
-        int count = 0;
-        for(int i = 0; i < nums.length-1; i++){
-            if(nums[i] == nums[i+1]) {
-                ans[count] = nums[i];
-                count++;
+        int idx = 0;
+
+        for (int num : nums) {
+            freq[num]++;
+
+            if (freq[num] == 2) {
+                ans[idx++] = num;
             }
         }
+
         return ans;
     }
 }
