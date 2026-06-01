fb24d9fc2149abe675c693d030016cbf3141ef36
Ankit Kumar Singh <148609982+SinghAnkitt@users.noreply.github.com>
2026-06-01 23:30:47 +0530

    Time: 1 ms (100.00%) | Memory: 44.5 MB (55.20%) - LeetSync

--- 3367-find-the-sum-of-encrypted-integers/find-the-sum-of-encrypted-integers.java
+++ 3367-find-the-sum-of-encrypted-integers/find-the-sum-of-encrypted-integers.java
@@ -1,33 +1,27 @@
 class Solution {
     public static int enc(int val){
         int k = val;
-        int max = 0;
-        int digits = 0;
-
+        int max = Integer.MIN_VALUE;
+        int count = 0;
         while(val > 0){
             int digit = val % 10;
             if(digit > max) max = digit;
             val /= 10;
-            digits++;
+            count++;
         }
-
         int encrypted = 0;
-        while(digits > 0){
-            encrypted = encrypted * 10 + max;
-            digits--;
+        while(count > 0){
+            encrypted = encrypted*10 + max;
+            count--;
         }
-
         return encrypted;
     }
-
     public int sumOfEncryptedInt(int[] nums) {
         int len = nums.length;
         int sum = 0;
-
         for(int i = 0; i < len; i++){
             sum += enc(nums[i]);
         }
-
         return sum;
     }
 }
