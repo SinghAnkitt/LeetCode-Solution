65153736ad6d2f722e2c17c6f69b6c813108a3bb
Ankit Kumar Singh <148609982+SinghAnkitt@users.noreply.github.com>
2026-06-04 11:08:14 +0530

    Time: 3 ms (93.48%) | Memory: 44.8 MB (81.88%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -3,9 +3,11 @@ class Solution {
         int[] ans = new int[nums1.length];
         for(int i = 0; i < nums1.length; i++){
             int index = -1;
-            int max = Integer.MIN_VALUE;
             for(int j = 0; j < nums2.length; j++){
-                if(nums1[i] == nums2[j]) index = j;
+                if(nums1[i] == nums2[j]) {
+                    index = j;
+                    break;
+                    }
             }
             for(int k = index; k < nums2.length; k++){
                 if(nums2[k] > nums1[i]) {
