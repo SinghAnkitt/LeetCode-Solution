f0c0956063f91c7f60fb33c7fe66d96fce5d07cf
Ankit Kumar Singh <148609982+SinghAnkitt@users.noreply.github.com>
2026-06-11 15:01:04 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (30.17%) - LeetSync

--- 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
@@ -1,16 +1,39 @@
+// class Solution {
+//     public static int gcd(int a, int b) {
+//     while (b != 0) {
+//         int temp = b;
+//         b = a % b;
+//         a = temp;
+//     }
+//     return a;
+// }
+//     public int findGCD(int[] nums) {
+//         Arrays.sort(nums);
+//         int i = nums[0];
+//         int j = nums[nums.length-1];
+//         return gcd(i, j);
+//     }
+// }
+
 class Solution {
-    public static int gcd(int a, int b) {
-    while (b != 0) {
-        int temp = b;
-        b = a % b;
-        a = temp;
-    }
-    return a;
-}
     public int findGCD(int[] nums) {
-        Arrays.sort(nums);
-        int i = nums[0];
-        int j = nums[nums.length-1];
-        return gcd(i, j);
+        int min = nums[0];
+        int max = nums[0];
+
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+
+        return gcd(min, max);
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
     }
 }
