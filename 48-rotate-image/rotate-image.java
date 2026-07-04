11a97b347b6c0a4eedc946e53824ab5c72040bc4
Ankit Kumar Singh <148609982+SinghAnkitt@users.noreply.github.com>
2026-07-04 18:01:46 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (15.59%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,21 +1,20 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int n = matrix.length;
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < i; j++){
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
         }
-        for (int i = 0; i < n; i++) {
-            int left = 0, right = n - 1;
-            while (left < right) {
-                int temp = matrix[i][left];
-                matrix[i][left] = matrix[i][right];
-                matrix[i][right] = temp;
-                left++;
-                right--;
+        for(int i = 0; i < matrix.length; i++){
+            int strCol = 0, endCol = matrix[0].length-1;
+            while(strCol < endCol){
+                int temp = matrix[i][strCol];
+                matrix[i][strCol] = matrix[i][endCol];
+                matrix[i][endCol]= temp;
+                strCol++;
+                endCol--;
             }
         }
     }
