1f8a8dfeab2f0caa3313d9f515bdeb6935a709ad
Ankit Kumar Singh <148609982+SinghAnkitt@users.noreply.github.com>
2026-07-09 20:34:00 +0530

    Time: 3 ms (97.63%) | Memory: 66.9 MB (57.50%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.java
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.java
@@ -3,7 +3,7 @@ class Solution {
         if(m * n != original.length){
             return new int[0][0];
         }
-        int[][] ans = new int[m][n];
+        int ans[][] = new int[m][n];
         int index = 0;
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
