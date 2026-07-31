cab491478b6aebcda1d74461d01eef9ef9e9f889
Ankit Kumar Singh <148609982+SinghAnkitt@users.noreply.github.com>
2026-07-31 21:51:12 +0530

    Time: 9 ms (94.22%) | Memory: 47.7 MB (94.51%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int minimumPushes(String word) {
+        int[] freq = new int[26];
+
+        for (char ch : word.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        Arrays.sort(freq);
+
+        int ans = 0;
+
+        for (int i = 25; i >= 0; i--) {
+            int index = 25 - i;        // 0,1,2,...
+            int cost = index / 8 + 1;  // 1,1,...,2,2,...
+            ans += freq[i] * cost;
+        }
+
+        return ans;
+    }
+}
