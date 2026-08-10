279a3673b055d0aa373117ddb06f9a9eacfe1a97
Ankit Kumar Singh <148609982+SinghAnkitt@users.noreply.github.com>
2026-08-11 00:02:01 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (39.13%) - LeetSync

--- 1307-ugly-number-iii/ugly-number-iii.java
+++ 1307-ugly-number-iii/ugly-number-iii.java
@@ -1,17 +1,13 @@
 class Solution {
     public int nthUglyNumber(int n, int a, int b, int c) {
-        
         long ab = lcm(a, b);
         long ac = lcm(a, c);
         long bc = lcm(b, c);
         long abc = lcm(ab, c);
-
         long left = 1;
         long right = 2_000_000_000L;
-
         while (left < right) {
             long mid = left + (right - left) / 2;
-
             long count = mid / a
                        + mid / b
                        + mid / c
@@ -19,17 +15,14 @@ class Solution {
                        - mid / ac
                        - mid / bc
                        + mid / abc;
-
             if (count >= n) {
                 right = mid;
             } else {
                 left = mid + 1;
             }
         }
-
         return (int) left;
     }
-
     private long gcd(long a, long b) {
         while (b != 0) {
             long temp = a % b;
@@ -38,7 +31,6 @@ class Solution {
         }
         return a;
     }
-
     private long lcm(long a, long b) {
         return (a / gcd(a, b)) * b;
     }
